d772e9c352777d436223442364e4afbe7bb70f33
Arturo Rodríguez <120765362+ArturoLevy@users.noreply.github.com>
2024-10-23 23:42:59 -0500

    Proyecto 1 completo

---
+++ Proyecto1/Proyecto1/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Proyecto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnNum1 = new Button();
+            btnNum2 = new Button();
+            btnNum3 = new Button();
+            btnNum4 = new Button();
+            btnNum5 = new Button();
+            btnNum6 = new Button();
+            btnNum7 = new Button();
+            btnNum8 = new Button();
+            btnNum9 = new Button();
+            btnPunto = new Button();
+            btnNum0 = new Button();
+            btnNumNegativo = new Button();
+            btnSum = new Button();
+            btnRes = new Button();
+            btnMul = new Button();
+            btnDiv = new Button();
+            btnRaiz = new Button();
+            btnPotencia = new Button();
+            btnC = new Button();
+            btnCE = new Button();
+            label1 = new Label();
+            txtDisplay = new TextBox();
+            btnResultado = new Button();
+            SuspendLayout();
+            // 
+            // btnNum1
+            // 
+            btnNum1.Location = new Point(197, 237);
+            btnNum1.Name = "btnNum1";
+            btnNum1.Size = new Size(57, 58);
+            btnNum1.TabIndex = 0;
+            btnNum1.Text = "1";
+            btnNum1.UseVisualStyleBackColor = true;
+            // 
+            // btnNum2
+            // 
+            btnNum2.Location = new Point(260, 237);
+            btnNum2.Name = "btnNum2";
+            btnNum2.Size = new Size(57, 58);
+            btnNum2.TabIndex = 1;
+            btnNum2.Text = "2";
+            btnNum2.UseVisualStyleBackColor = true;
+            // 
+            // btnNum3
+            // 
+            btnNum3.Location = new Point(323, 237);
+            btnNum3.Name = "btnNum3";
+            btnNum3.Size = new Size(57, 58);
+            btnNum3.TabIndex = 2;
+            btnNum3.Text = "3";
+            btnNum3.UseVisualStyleBackColor = true;
+            // 
+            // btnNum4
+            // 
+            btnNum4.Location = new Point(197, 301);
+            btnNum4.Name = "btnNum4";
+            btnNum4.Size = new Size(57, 58);
+            btnNum4.TabIndex = 3;
+            btnNum4.Text = "4";
+            btnNum4.UseVisualStyleBackColor = true;
+            // 
+            // btnNum5
+            // 
+            btnNum5.Location = new Point(260, 301);
+            btnNum5.Name = "btnNum5";
+            btnNum5.Size = new Size(57, 58);
+            btnNum5.TabIndex = 4;
+            btnNum5.Text = "5";
+            btnNum5.UseVisualStyleBackColor = true;
+            // 
+            // btnNum6
+            // 
+            btnNum6.Location = new Point(323, 301);
+            btnNum6.Name = "btnNum6";
+            btnNum6.Size = new Size(57, 58);
+            btnNum6.TabIndex = 5;
+            btnNum6.Text = "6";
+            btnNum6.UseVisualStyleBackColor = true;
+            // 
+            // btnNum7
+            // 
+            btnNum7.Location = new Point(197, 365);
+            btnNum7.Name = "btnNum7";
+            btnNum7.Size = new Size(57, 58);
+            btnNum7.TabIndex = 6;
+            btnNum7.Text = "7";
+            btnNum7.UseVisualStyleBackColor = true;
+            // 
+            // btnNum8
+            // 
+            btnNum8.Location = new Point(260, 365);
+            btnNum8.Name = "btnNum8";
+            btnNum8.Size = new Size(57, 58);
+            btnNum8.TabIndex = 7;
+            btnNum8.Text = "8";
+            btnNum8.UseVisualStyleBackColor = true;
+            // 
+            // btnNum9
+            // 
+            btnNum9.Location = new Point(323, 365);
+            btnNum9.Name = "btnNum9";
+            btnNum9.Size = new Size(57, 58);
+            btnNum9.TabIndex = 8;
+            btnNum9.Text = "9";
+            btnNum9.UseVisualStyleBackColor = true;
+            // 
+            // btnPunto
+            // 
+            btnPunto.Location = new Point(197, 429);
+            btnPunto.Name = "btnPunto";
+            btnPunto.Size = new Size(57, 58);
+            btnPunto.TabIndex = 9;
+            btnPunto.Text = ".";
+            btnPunto.UseVisualStyleBackColor = true;
+            // 
+            // btnNum0
+            // 
+            btnNum0.Location = new Point(260, 429);
+            btnNum0.Name = "btnNum0";
+            btnNum0.Size = new Size(57, 58);
+            btnNum0.TabIndex = 10;
+            btnNum0.Text = "0";
+            btnNum0.UseVisualStyleBackColor = true;
+            // 
+            // btnNumNegativo
+            // 
+            btnNumNegativo.Location = new Point(323, 429);
+            btnNumNegativo.Name = "btnNumNegativo";
+            btnNumNegativo.Size = new Size(57, 58);
+            btnNumNegativo.TabIndex = 11;
+            btnNumNegativo.Text = "-";
+            btnNumNegativo.UseVisualStyleBackColor = true;
+            btnNumNegativo.Click += btnNumNegativo_Click;
+            // 
+            // btnSum
+            // 
+            btnSum.Location = new Point(407, 301);
+            btnSum.Name = "btnSum";
+            btnSum.Size = new Size(57, 58);
+            btnSum.TabIndex = 12;
+            btnSum.Text = "+";
+            btnSum.UseVisualStyleBackColor = true;
+            // 
+            // btnRes
+            // 
+            btnRes.Location = new Point(407, 365);
+            btnRes.Name = "btnRes";
+            btnRes.Size = new Size(57, 58);
+            btnRes.TabIndex = 13;
+            btnRes.Text = "-";
+            btnRes.UseVisualStyleBackColor = true;
+            // 
+            // btnMul
+            // 
+            btnMul.Location = new Point(470, 301);
+            btnMul.Name = "btnMul";
+            btnMul.Size = new Size(57, 58);
+            btnMul.TabIndex = 14;
+            btnMul.Text = "x";
+            btnMul.UseVisualStyleBackColor = true;
+            // 
+            // btnDiv
+            // 
+            btnDiv.Location = new Point(470, 365);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(57, 58);
+            btnDiv.TabIndex = 15;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = true;
+            // 
+            // btnRaiz
+            // 
+            btnRaiz.Location = new Point(407, 429);
+            btnRaiz.Name = "btnRaiz";
+            btnRaiz.Size = new Size(57, 58);
+            btnRaiz.TabIndex = 16;
+            btnRaiz.Text = "√";
+            btnRaiz.UseVisualStyleBackColor = true;
+            // 
+            // btnPotencia
+            // 
+            btnPotencia.Location = new Point(470, 429);
+            btnPotencia.Name = "btnPotencia";
+            btnPotencia.Size = new Size(57, 58);
+            btnPotencia.TabIndex = 17;
+            btnPotencia.Text = "^";
+            btnPotencia.UseVisualStyleBackColor = true;
+            // 
+            // btnC
+            // 
+            btnC.Location = new Point(407, 237);
+            btnC.Name = "btnC";
+            btnC.Size = new Size(57, 58);
+            btnC.TabIndex = 18;
+            btnC.Text = "C";
+            btnC.UseVisualStyleBackColor = true;
+            btnC.Click += btnC_Click;
+            // 
+            // btnCE
+            // 
+            btnCE.Location = new Point(470, 237);
+            btnCE.Name = "btnCE";
+            btnCE.Size = new Size(57, 58);
+            btnCE.TabIndex = 19;
+            btnCE.Text = "CE";
+            btnCE.UseVisualStyleBackColor = true;
+            btnCE.Click += btnCE_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(341, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 20);
+            label1.TabIndex = 21;
+            label1.Text = "CALCULADORA DS4";
+            // 
+            // txtDisplay
+            // 
+            txtDisplay.Location = new Point(197, 129);
+            txtDisplay.Multiline = true;
+            txtDisplay.Name = "txtDisplay";
+            txtDisplay.Size = new Size(451, 102);
+            txtDisplay.TabIndex = 22;
+            // 
+            // btnResultado
+            // 
+            btnResultado.Location = new Point(533, 237);
+            btnResultado.Name = "btnResultado";
+            btnResultado.Size = new Size(115, 250);
+            btnResultado.TabIndex = 23;
+            btnResultado.Text = "=";
+            btnResultado.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(793, 532);
+            Controls.Add(btnResultado);
+            Controls.Add(txtDisplay);
+            Controls.Add(label1);
+            Controls.Add(btnCE);
+            Controls.Add(btnC);
+            Controls.Add(btnPotencia);
+            Controls.Add(btnRaiz);
+            Controls.Add(btnDiv);
+            Controls.Add(btnMul);
+            Controls.Add(btnRes);
+            Controls.Add(btnSum);
+            Controls.Add(btnNumNegativo);
+            Controls.Add(btnNum0);
+            Controls.Add(btnPunto);
+            Controls.Add(btnNum9);
+            Controls.Add(btnNum8);
+            Controls.Add(btnNum7);
+            Controls.Add(btnNum6);
+            Controls.Add(btnNum5);
+            Controls.Add(btnNum4);
+            Controls.Add(btnNum3);
+            Controls.Add(btnNum2);
+            Controls.Add(btnNum1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnNum1;
+        private Button btnNum2;
+        private Button btnNum3;
+        private Button btnNum4;
+        private Button btnNum5;
+        private Button btnNum6;
+        private Button btnNum7;
+        private Button btnNum8;
+        private Button btnNum9;
+        private Button btnPunto;
+        private Button btnNum0;
+        private Button btnNumNegativo;
+        private Button btnSum;
+        private Button btnRes;
+        private Button btnMul;
+        private Button btnDiv;
+        private Button btnRaiz;
+        private Button btnPotencia;
+        private Button btnC;
+        private Button btnCE;
+        private Label label1;
+        private TextBox txtDisplay;
+        private Button btnResultado;
+    }
+}
---
+++ Proyecto1/Proyecto1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace Proyecto1
+{
+    public partial class Form1 : Form
+    {
+        string operacion = "";           
+        double valor1 = 0;               
+        bool operacionPresionada = false;
+        string connectionString = "Server=ARTURO\\SQLEXPRESS;Database=Proyecto1_DS4;Trusted_Connection=True;"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnNum1.Click += new EventHandler(BotonNumero_Click);
+            btnNum2.Click += new EventHandler(BotonNumero_Click);
+            btnNum3.Click += new EventHandler(BotonNumero_Click);
+            btnNum4.Click += new EventHandler(BotonNumero_Click);
+            btnNum5.Click += new EventHandler(BotonNumero_Click);
+            btnNum6.Click += new EventHandler(BotonNumero_Click);
+            btnNum7.Click += new EventHandler(BotonNumero_Click);
+            btnNum8.Click += new EventHandler(BotonNumero_Click);
+            btnNum9.Click += new EventHandler(BotonNumero_Click);
+            btnNum0.Click += new EventHandler(BotonNumero_Click);
+
+            btnSum.Click += new EventHandler(BotonOperacion_Click);
+            btnRes.Click += new EventHandler(BotonOperacion_Click);
+            btnMul.Click += new EventHandler(BotonOperacion_Click);
+            btnDiv.Click += new EventHandler(BotonOperacion_Click);
+            btnRaiz.Click += new EventHandler(BotonOperacion_Click);
+            btnPotencia.Click += new EventHandler(BotonOperacion_Click);
+
+            btnResultado.Click += new EventHandler(botonResultado_Click);
+
+            btnPunto.Click += new EventHandler(BotonPunto_Click);
+
+        }
+
+        private void BotonNumero_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button boton)
+            {
+                if (txtDisplay.Text == "0" || operacionPresionada)
+                {
+                    txtDisplay.Clear(); 
+                }
+
+                operacionPresionada = false;
+                txtDisplay.Text += boton.Text;  
+            }
+        }
+
+        private void BotonOperacion_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button boton)
+            {
+                operacion = boton.Text; 
+                valor1 = Double.Parse(txtDisplay.Text); 
+                operacionPresionada = true;
+            }
+        }
+
+        private void botonResultado_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                decimal numero1 = Convert.ToDecimal(valor1);
+                decimal numero2 = Convert.ToDecimal(txtDisplay.Text);
+                string operacion = this.operacion;
+                decimal resultado;
+
+                switch (operacion)
+                {
+                    case "+":
+                        resultado = numero1 + numero2;
+                        break;
+                    case "-":
+                        resultado = numero1 - numero2;
+                        break;
+                    case "x":
+                        resultado = numero1 * numero2;
+                        break;
+                    case "/":
+                        resultado = numero1 / numero2;
+                        break;
+                    case "√":  
+                        resultado = (decimal)Math.Sqrt((double)numero1);
+                        break;
+                    case "^": 
+                        resultado = (decimal)Math.Pow((double)numero1, (double)numero2);
+                        break;
+                    default:
+                        resultado = 0;
+                        break;
+                }
+
+            
+                txtDisplay.Text = resultado.ToString();
+
+                GuardarCalculoEnBaseDeDatos(numero1, operacion, numero2, resultado);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en el cálculo: " + ex.Message);
+            }
+        }
+
+        private void BotonPunto_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button boton)
+            {
+                if (!txtDisplay.Text.Contains('.'))
+                {
+                    txtDisplay.Text += "."; 
+                }
+            }
+        }
+        private void GuardarCalculoEnBaseDeDatos(decimal numero1, string operacion, decimal numero2, decimal resultado)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO CalculadoraCientifica " + "(Numero1, Operacion, Numero2, Resultado, FechaCalculo) VALUES (@numero1, @operacion, @numero2, @resultado, @fecha)";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@numero1", numero1);
+                    command.Parameters.AddWithValue("@operacion", operacion);
+                    command.Parameters.AddWithValue("@numero2", numero2);
+                    command.Parameters.AddWithValue("@resultado", resultado);
+                    command.Parameters.AddWithValue("@fecha", DateTime.Now);
+
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Guardado en la Base de Datos Proyecto1_DS4");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar cálculo: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnCE_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+            valor1 = 0;
+        }
+
+        private void btnNumNegativo_Click(object sender, EventArgs e)
+        {
+            double numero = Double.Parse(txtDisplay.Text);
+            numero = numero * -1;
+            txtDisplay.Text = numero.ToString();
+        }
+    }
+}
